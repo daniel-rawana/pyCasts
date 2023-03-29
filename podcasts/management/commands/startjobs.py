7b8f35d8387af0ed6a4a1fc65b35481d7470e908
daniel-rawana <daniel_rawana@outlook.com>
2023-03-29 18:21:48 -0400

    Updated custom job to parse two separate podcast feeds with reusable code to easily add more fields

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -5,12 +5,20 @@ from dateutil import parser
 
 from podcasts.models import Episode
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
+def save_new_episodes(feed):
+    """Saves new episodes to the database
 
+    Checks the episode GUID against the episodes currently stored in the
+    database. If not found, then a new `Episode` is added to the database.
+
+    Args:
+        feed: requires a feedparser object
+    
+    """
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
         for item in feed.entries:
             if not Episode.objects.filter(guid=item.guid).exists():
                 episode = Episode(
@@ -23,3 +31,21 @@ class Command(BaseCommand):
                     guid=item.guid,
                 )
                 episode.save()
+
+
+def fetch_realpython_episodes():
+    """Fetches new episodes from RSS for The Real Python Podcast."""
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+def fetch_talkpython_episodes():
+    """Fetches new episodes from RSS for the Talk Python to Me Podcast."""
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(_feed)
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        fetch_realpython_episodes()
+        fetch_talkpython_episodes()
+
+        
