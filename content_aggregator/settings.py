06622608bcdca5f3fc88c3bd6db737490dcb5931
daniel-rawana <daniel_rawana@outlook.com>
2023-03-30 01:32:37 -0400

    Created job to clean up job history after one week, added jobs to scheduler

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # My Apps
     "podcasts.apps.PodcastsConfig",
+    # Third Party Apps
+    "django_apscheduler"
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,17 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers":["console"],
+        "level": "INFO",
+    },
+}
--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -3,7 +3,25 @@ from django.core.management.base import BaseCommand
 import feedparser
 from dateutil import parser
 
+# Standard Library
+import logging
+
+# Django
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+# Third Party
+import feedparser
+from dateutil import parser
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+# Models
 from podcasts.models import Episode
+
+logger = logging.getLogger(__name__)
 
 def save_new_episodes(feed):
     """Saves new episodes to the database
@@ -43,9 +61,53 @@ def fetch_talkpython_episodes():
     _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
     save_new_episodes(_feed)
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        fetch_realpython_episodes()
-        fetch_talkpython_episodes()
+def delete_old_job_executions(max_age=604_800):
+    """Deletes all apscheduler job execution logs older 'than max_age'."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
+class Command(BaseCommand):
+    help = "Runs apscheduler"
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            fetch_realpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="The Real Python Podcast",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: The Real Python Podcast.")
+
+        scheduler.add_job(
+            fetch_talkpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="Talk Python Feed",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added Job: Talk Python Feed.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ), # Delete old jobs at Midnight on monday
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: Delete Old Job Executions")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
         
